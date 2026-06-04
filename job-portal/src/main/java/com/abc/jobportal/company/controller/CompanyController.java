afcaa81586f6e004553e988a65818a11f8a3992c
cham207388 <cham207388@gmail.com>
2026-06-04 13:57:43 -0500

    db params

--- job-portal/src/main/java/com/abc/jobportal/company/controller/CompanyController.java
+++ job-portal/src/main/java/com/abc/jobportal/company/controller/CompanyController.java
@@ -1,16 +1,26 @@
 package com.abc.jobportal.company.controller;
 
+import com.abc.jobportal.service.ICompanyService;
+import com.abc.jobportal.dto.CompanyDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/companies")
+@RequiredArgsConstructor
 public class CompanyController {
 
+    private final ICompanyService companyService;
+
     @GetMapping(version = "1.0")
-    public ResponseEntity<String> getCompaniesV1() {
-        return ResponseEntity.ok("List of companies - API Version 1.0");
+    public ResponseEntity<List<CompanyDto>> getAllCompanies() {
+        List<CompanyDto> companyList = companyService.getAllCompanies();
+        return ResponseEntity.ok().body(companyList);
     }
+
 }
