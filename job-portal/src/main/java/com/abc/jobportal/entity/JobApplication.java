58d9564e692c4c82557d38c98f41ed91fc627591
cham207388 <cham207388@gmail.com>
2026-07-10 18:23:21 -0500

    refactor: remove unused JPA annotations from JobApplication and Profile entities

--- job-portal/src/main/java/com/abc/jobportal/entity/JobApplication.java
+++ job-portal/src/main/java/com/abc/jobportal/entity/JobApplication.java
@@ -1,7 +1,5 @@
 package com.abc.jobportal.entity;
 
-import jakarta.persistence.AttributeOverride;
-import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,7 +7,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
